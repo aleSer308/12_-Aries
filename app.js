fdd8ac7f1851a101bef41d11055d4a9589d2ebd3
aleSer308 <alegresergio@hotmail.com.ar>
2022-11-16 20:54:10 -0300

    copia de Aries

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000
+
+//Motor de vistas
+app.set('view engine', 'ejs'); 
+
+//Levantando servidor
+app.listen(PORT, () => console.log('Servidor corriendo en el puerto' + PORT));
+
+//Rutas
+app.get('/', (req, res) => {
+    res.render(path.join(__dirname, '/views/index'))
+});
+
+app.get('/login', (req, res) => {
+    res.render(path.join(__dirname, '/views/login'))
+});
+
+app.get('/register', (req, res) => {
+    res.render(path.join(__dirname, '/views/register'))
+});
+
+app.get('/produCtart', (req, res)=>{
+    res.render(path.join(__dirname, '/views/produCtart'))
+});
+
+app.get('/detalle', (req, res)=>{
+    res.render(path.join(__dirname, '/views/product-detail'))
+});
+
+//Recursos estaticos
+app.use(express.static('public'));
